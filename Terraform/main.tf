995aa7ad5cf1b287758b1e563782a94f19ea5884
ItsGibbs <thepapeyeku@gmail.com>
2023-04-05 11:24:31 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,7 +12,39 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "vpc_cloudy" {
+  cidr_block = "172.31.0.0/16"
+}
+
+resource "aws_subnet" "subnet_cloudy" {
+  vpc_id            = aws_vpc.vpc_cloudy.id
+  cidr_block        = "172.31.10.0/24"
+  availability_zone = "eu-west-3"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
 }

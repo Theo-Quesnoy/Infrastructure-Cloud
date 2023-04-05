86d3ec12782fcd7437ac1d5ed24f596d1387f511
ItsGibbs <thepapeyeku@gmail.com>
2023-04-05 11:32:00 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -20,27 +20,38 @@ resource "aws_subnet" "subnet_cloudy" {
   vpc_id            = aws_vpc.vpc_cloudy.id
   cidr_block        = "172.31.10.0/24"
   availability_zone = "eu-west-3"
-
-  tags = {
-    Name = "tf-example"
-  }
 }
 
-resource "aws_network_interface" "foo" {
+resource "aws_network_interface" "node1" {
   subnet_id   = aws_subnet.subnet_cloudy.id
   private_ips = ["172.16.10.100"]
-
-  tags = {
-    Name = "primary_network_interface"
-  }
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
+resource "aws_network_interface" "node2" {
+  subnet_id   = aws_subnet.subnet_cloudy.id
+  private_ips = ["172.16.10.200"]
+}
+
+resource "aws_instance" "node1" {
+  ami           = "ami-002ff2c881c910aa8" # eu-west-3
   instance_type = "t2.micro"
 
   network_interface {
-    network_interface_id = aws_network_interface.foo.id
+    network_interface_id = aws_network_interface.node1.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+}
+
+resource "aws_instance" "node2" {
+  ami           = "ami-002ff2c881c910aa8" # eu-west-3
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.node2.id
     device_index         = 0
   }
 

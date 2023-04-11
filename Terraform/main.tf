a63769ec78d8eb15ba84dab2cc8fdd0aa100897a
ItsGibbs <thepapeyeku@gmail.com>
2023-04-11 11:49:52 +0200

    update + ignore exe

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -19,17 +19,17 @@ resource "aws_vpc" "vpc_cloudy" {
 resource "aws_subnet" "subnet_cloudy" {
   vpc_id            = aws_vpc.vpc_cloudy.id
   cidr_block        = "172.31.10.0/24"
-  availability_zone = "eu-west-3"
+  availability_zone = "eu-west-3c"
 }
 
 resource "aws_network_interface" "node1" {
   subnet_id   = aws_subnet.subnet_cloudy.id
-  private_ips = ["172.16.10.100"]
+  private_ips = ["172.31.10.100"]
 }
 
 resource "aws_network_interface" "node2" {
   subnet_id   = aws_subnet.subnet_cloudy.id
-  private_ips = ["172.16.10.200"]
+  private_ips = ["172.31.10.200"]
 }
 
 resource "aws_instance" "node1" {
@@ -39,6 +39,10 @@ resource "aws_instance" "node1" {
   network_interface {
     network_interface_id = aws_network_interface.node1.id
     device_index         = 0
+  }
+
+  tags = {
+    Name = "Node1"
   }
 
   credit_specification {
@@ -53,6 +57,10 @@ resource "aws_instance" "node2" {
   network_interface {
     network_interface_id = aws_network_interface.node2.id
     device_index         = 0
+  }
+
+  tags = {
+    Name = "Node2"
   }
 
   credit_specification {
